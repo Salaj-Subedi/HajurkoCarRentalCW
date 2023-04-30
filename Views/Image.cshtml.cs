ba0bd680bdebcd4d9f1f9578cdc2d2f3ba787661
Salaj Subedi <SalajSubedi11@gmail.com>
2023-04-30 16:57:16 +0545

    features updated in workings

---
+++ Views/Image.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HajurKoCarRental.Views
+{
+    public class ImageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
