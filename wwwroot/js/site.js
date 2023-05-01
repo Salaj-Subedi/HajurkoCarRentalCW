f8e7da98e8281c0cc5bc3cc564afbf5e20e97bc1
Krish Zayn <krishzayn7@gmail.com>
2023-05-01 22:11:56 +0545

    feat: user car cards

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const cardsContainer = document.querySelector('.cards-container');
+const cards = document.querySelectorAll('.card');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const cardWidth = cards[0].offsetWidth;
+const cardMarginRight = parseInt(window.getComputedStyle(cards[0]).marginRight);
+
+let currentCardIndex = 0;
+let cardTranslateX = 0;
+
+cardsContainer.style.width = `${(cardWidth + cardMarginRight) * cards.length - cardMarginRight}px`;
+
+function slideCards() {
+    cardsContainer.style.transform = `translateX(-${cardTranslateX}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    if (currentCardIndex < cards.length - 1) {
+        currentCardIndex++;
+        cardTranslateX = currentCardIndex * (cardWidth + cardMarginRight);
+        slideCards();
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentCardIndex > 0) {
+        currentCardIndex--;
+        cardTranslateX = currentCardIndex * (cardWidth + cardMarginRight);
+        slideCards();
+    }
+});
